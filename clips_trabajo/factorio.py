1bd25bcf1136131f092451df11d1de4f560112b7
Aurora Zuoris <programming.aurora@gmail.com>
2023-05-09 12:02:30 +0100

    Work on everything

--- clips_trabajo/factorio.py
+++ clips_trabajo/factorio.py
@@ -1,4 +1,34 @@
+from collections import defaultdict
 import clips
+
+class ProductionTree:
+    def __init__(self, item, ammount):
+        self.item = item
+        self.ammount = ammount
+        self.children = []
+    
+    def add_child(self, child):
+        self.children.append(child)
+    
+    def print_tree(self):
+        stack = [(self, 0)]
+        while stack:
+            node, level = stack.pop()
+            print('| ' * level + node.item + ' ' + str(node.ammount))
+            for child in node.children:
+                stack.append((child, level + 1))
+    
+    def __repr__(self):
+        return f'ProductionTree({self.item}, {self.ammount})'
+
+def create_tree(tree_facts):
+    fact_map = {f[2]: ProductionTree(f[0], f[1]) for f in tree_facts}
+    for f in tree_facts:
+        if f[3] != 'nil':
+            fact_map[f[3]].add_child(fact_map[f[2]])
+    root_node = next(fact_map[f[2]] for f in tree_facts if f[3] == 'nil')
+    return root_node
+    
 
 class FactorioCalc:
 
@@ -12,5 +42,6 @@ class FactorioCalc:
         self.env.reset()
         self.env.assert_string(f'(request "{item}" {amount})')
         self.env.run()
-        return self.env.facts()
-    
+        prod_tree_facts = [f for f in self.env.facts() if f.template.name == 'request_tree']
+        return prod_tree_facts
+        return create_tree(prod_tree_facts)
